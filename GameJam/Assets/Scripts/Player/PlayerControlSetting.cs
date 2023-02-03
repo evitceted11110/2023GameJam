724dba1386d59c19e7e349684782fc7794b64fb0
SAMUEL <samuellin@yile.com.tw>
2023-02-04 00:13:15 +0800

    新增 TMP 基礎檔案

---
+++ GameJam/Assets/Scripts/Player/PlayerControlSetting.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlSetting : ScriptableObject
+{
+    public float moveSpeed;
+    public float jumpHeight;
+    public float throwStrength;
+}

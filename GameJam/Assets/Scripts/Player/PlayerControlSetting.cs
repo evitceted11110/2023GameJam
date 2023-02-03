71d8040153140bf34d2ff82f2e90abeb6751a277
SAMUEL <samuellin@yile.com.tw>
2023-02-04 00:19:03 +0800

    修正 Controller Error

--- GameJam/Assets/Scripts/Player/PlayerControlSetting.cs
+++ GameJam/Assets/Scripts/Player/PlayerControlSetting.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerControlSetting : ScriptableObject
 {
+    public string upKey;
+    public string downKey;
+    public string rightKey;
+    public string leftKey;      
+    public string actionKey;    //丟擲/撿取
+    public string talkKey;      //互動/對話
+
     public float moveSpeed;
     public float jumpHeight;
     public float throwStrength;

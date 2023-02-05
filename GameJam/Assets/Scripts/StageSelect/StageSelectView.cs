6f87393f7e5bf39dbb5b5fb5d56415350add612a
Sun <qaz87765@gmail.com>
2023-02-05 10:55:38 +0800

    1.新增選關按鈕邏輯

--- GameJam/Assets/Scripts/StageSelect/StageSelectView.cs
+++ GameJam/Assets/Scripts/StageSelect/StageSelectView.cs
@@ -1,8 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI.Extensions.Tweens;
+using Newtonsoft.Json;
 public class StageSelectView : MonoBehaviour
 {
-    
+    public SingleStageSelector[] stageButtonList;
+    private void Awake()
+    {
+        InitStageButtonDateSetup();
+    }
+
+    public void InitStageButtonDateSetup()
+    {
+        PlayerPrefsService.Instance.RefreshUseTime();
+        float[] bestTimes = PlayerPrefsService.Instance.bestTimes;
+        if (bestTimes == null)
+        {
+            PlayerPrefsService.Instance.bestTimes = new float[stageButtonList.Length];
+        }
+        StageSetting stageSetting = StageManager.Instance.GetStageSetting();
+        for (int x = 0; x < stageButtonList.Length; x++)
+        {
+            bool[] starResult = stageSetting.GetStarResult(bestTimes[x]);
+            for(int y = 0; y < starResult.Length; y++)
+            {
+                if (starResult[y])
+                    stageButtonList[x].stars[y].SetActive(true);
+                else
+                    stageButtonList[x].stars[y].SetActive(false);
+            }
+        }
+    }
 }

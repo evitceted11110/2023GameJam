0c85d86dee934071432b4afc56d24829e6b7f07a
SAMUEL <samuellin@yile.com.tw>
2023-02-05 10:12:22 +0800

    新增 結果星數設定

--- GameJam/Assets/Scripts/StageManager/StageSetting.cs
+++ GameJam/Assets/Scripts/StageManager/StageSetting.cs
@@ -9,4 +9,29 @@ public class StageSetting : ScriptableObject
     public GameObject[] createObjects;
     public List<ItemBase> leftProductItems;
     public List<ItemBase> rightProductItems;
+    //限制時間
+    public float totalTime;
+
+    //使用時間為基礎
+    //ex 限制時間為 300s
+    //[0] = 300
+    //[1] = 240
+    //[3] = 180
+    public float[] starGap;
+
+    public bool[] GetStarResult(float passTime)
+    {
+        if (passTime == -1)
+        {
+            return new bool[starGap.Length];
+        }
+
+        List<bool> resultStar = new List<bool>();
+
+        for (int i = 0; i < starGap.Length; i++)
+        {
+            resultStar.Add(passTime < starGap[i]);
+        }
+        return resultStar.ToArray();
+    }
 }
